a8f5e10e518db031dbfee451bc43b5e5bd813e2b
Green Creative <greencreative@Greens-iMac-85.local>
2020-09-18 12:23:56 +0100

    Cleaned styling

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const Stack = createStackNavigator();
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.body}>
-      <Text>Home</Text>
+      <Text style={styles.text}>Home</Text>
       <Button title='Scan now' onPress={() => {
         navigation.navigate('ScannerScreen');
       }} />
@@ -31,16 +31,14 @@ const ScannerScreen = () => {
       onRead={this.onSuccess}
       flashMode={RNCamera.Constants.FlashMode.torch}
       topContent={
-        <Text style={styles.centerText}>
+        <Text style={styles.text}>
           Go to{' '}
           <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
           your computer and scan the QR code.
         </Text>
       }
       bottomContent={
-        <TouchableOpacity style={styles.buttonTouchable}>
-          <Text style={styles.buttonText}>OK. Got it!</Text>
-        </TouchableOpacity>
+        <Button title='OK. Got it!' />
       }
     />
   );
@@ -65,21 +63,12 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   text: {
-    fontSize: 40
-  },
-  centerText: {
-    flex: 1,
-    fontSize: 18,
-    padding: 32,
-    color: '#777'
+    fontSize: 16,
+    margin: 10
   },
   textBold: {
     fontWeight: '500',
     color: '#000'
-  },
-  buttonText: {
-    fontSize: 21,
-    color: 'rgb(0,122,255)'
   },
   buttonTouchable: {
     padding: 16
